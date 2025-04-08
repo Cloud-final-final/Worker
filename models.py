51e749587400b1ffb940c724c2f5b89edd731dfc
Daniel Fuentes <d.fuentese@uniandes.edu.co>
2025-04-08 08:55:53 -0500

    agregacion de models

---
+++ models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, String, ForeignKey, Text, JSON
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    username = Column(String, primary_key=True, index=True)
+    hashed_password = Column(String)
+
+    documents = relationship("Document", back_populates="owner")
+
+class Document(Base):
+    __tablename__ = "documents"
+    id = Column(String, primary_key=True, index=True)
+    owner_username = Column(String, ForeignKey("users.username"))
+    filename = Column(String)
+    file_path = Column(Text)
+    embeddings = Column(JSON)
+
+    owner = relationship("User", back_populates="documents")
